18c391b6f21b7f38293ce6206c5a7a02cf7f1b36
fountaina <fountainaigbona@gmail.com>
2025-03-23 19:49:22 +0100

    add working carousel buttons

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,8 +3,8 @@ import man1 from "/images/Carousel/man1.jpg";
 import man2 from "/images/Carousel/man2.jpg";
 import woman1 from "/images/Carousel/woman1.jpg";
 import "../styles/carousel.css"
-import nextSlideIcon from "/images/icons/right-arrow.png";
-import prevSlideIcon from "/images/icons/left-arrow.png";
+import nextSlideIcon from "/images/icons/right-white.png";
+import prevSlideIcon from "/images/icons/left-white.png";
 
 const Carousel = () => {
     const carouselImages = [man1, man2, woman1];
@@ -12,7 +12,7 @@ const Carousel = () => {
     // A state that handles the current position in the carousel images array
     const [position, setPosition] = useState(0);
 
-    function handleChange() {
+    function handleNext() {
         setPosition((prevPosition) => {
             // using a operator wrapping that will automically return 0 (beginning) when the full length of images have been reached
             const newPosition = (prevPosition + 1) % carouselImages.length;
@@ -20,12 +20,19 @@ const Carousel = () => {
         })
     }
 
+    function handlePrev() {
+        setPosition((prevPosition) => {
+            const newPosition = prevPosition === 0 ? carouselImages.length - 1 : (prevPosition - 1) % carouselImages.length;
+            return newPosition;
+        })
+    }
+
     return (
         <div className='carousel-image-container'>
-            <img className="carousel-image" onClick={handleChange} src={carouselImages[position]} alt="" />
-            <div className="carousel-slide-icon">
-                {/* <button className='carousel-slide-icon next'><img src={nextSlideIcon} alt="next" /></button> */}
-                {/* <button className='carousel-slide-icon prev'><img src={prevSlideIcon} alt="prev" /></button> */}
+            <img className="carousel-image"  src={carouselImages[position]} alt="" />
+            <div className="carousel-icon-container">
+                <button className='carousel-slide-icon prev'><img src={prevSlideIcon} alt="prev" onClick={handlePrev} /></button>
+                <button className='carousel-slide-icon next'><img src={nextSlideIcon} alt="next" onClick={handleNext} /></button>
             </div>
         </div>
     )
